49cb5c6c34afab67061f883dd867caeb683deced
abdalllah22 <ahmed.abdalllah22@gmail.com>
2021-07-26 14:20:27 +0200

    add helloApiView

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'profiles_api',
+    'rest_framework',
+    'rest_framework.authtoken',
+
 ]
 
 MIDDLEWARE = [
